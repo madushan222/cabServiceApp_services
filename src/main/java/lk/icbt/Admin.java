75f6911d1f0ac5ffe67f0622523166347de2690c
madushan222 <shanrox111@gmail.com>
2022-09-12 13:21:39 +0530

    Back End Coding

---
+++ src/main/java/lk/icbt/Admin.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.icbt;
+
+/**
+ *
+ * @author shanr
+ */
+public class Admin {
+    private String fName,lname,nic,dob,email,username,password;
+    private int mobile,branchId;
+    
+    public Admin() {
+        this.fName = "";
+        this.lname = "";
+        this.nic = "";
+        this.dob = "";
+        this.email = "";
+        this.username = "";
+        this.password = "";
+        this.mobile = 0;
+        this.branchId = 0;
+    }
+
+    public Admin(String fName, String lname, String nic, String dob, String email, String username, String password, int mobile, int branchId) {
+        this.fName = fName;
+        this.lname = lname;
+        this.nic = nic;
+        this.dob = dob;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.mobile = mobile;
+        this.branchId = branchId;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+    
+    
+}
---
+++ src/main/java/lk/icbt/UserService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package lk.icbt;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author shanr
+ */
+@WebService(serviceName = "UserService")
+public class UserService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "addAdmin")
+    public boolean addAdmin(@WebParam(name = "admin") Admin admin){
+        DBUtil util = new DBUtil();
+        return util.addAdmin(admin);
+    }
+            
+}
